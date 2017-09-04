f6a73c01805960d44185885a236bfad58546d5cf
Brian Heim <brianlheim@gmail.com>
2017-09-04 13:43:39 -0400

    travis test script: export SCLANG_CONF
    also add some extra echos for clarity

--- .travis/run-sc-class-library-test-suite.sh
+++ .travis/run-sc-class-library-test-suite.sh
@@ -5,18 +5,21 @@
 # as are tests for the UnitTesting Quark itself.
 
 echo "Attempting to configure test environment."
-SCLANG_CONF="$TRAVIS_BUILD_DIR/.travis/sclang_conf.yaml"
+export SCLANG_CONF="$TRAVIS_BUILD_DIR/.travis/sclang_conf.yaml"
 "$SCLANG" -i none "$TRAVIS_BUILD_DIR/.travis/configureTestEnvironment.scd"
 sleep 1
+
+echo "Testing for existence of $SCLANG_CONF"
 if [[ -e "$SCLANG_CONF" ]]
 then
 	echo "Configuration complete. Attempting to run unit tests now."
 	echo "sclang configuration: "
 	cat "$SCLANG_CONF"
+	echo ""
 	"$SCLANG" -i none -l "$SCLANG_CONF" "$TRAVIS_BUILD_DIR/.travis/runAllUnitTests.scd"
 	exit $?
 else
-	echo "Error while configuring test environment."
+	echo "ERROR: $SCLANG_CONF not found. Test environment configuration failed."
 	exit 1
 fi
 
