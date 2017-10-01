8d6e152eb07d5846fc1eb48aa7249d7e1816422d
Brian Heim <brianlheim@gmail.com>
2017-09-30 22:59:06 -0400

    travis: update qpm-test.sh script

--- .travis/qpm-test.sh
+++ .travis/qpm-test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-qpm test.run -l $TRAVIS_BUILD_DIR/BUILD/travis_test_run.json --path $SCLANG --include $HOME/Quarks \
+qpm test.run -l $TRAVIS_BUILD_DIR/.travis/travis_test_run.json --path $SCLANG \
+    --include $HOME/Quarks \
     --include $TRAVIS_BUILD_DIR/testsuite/classlibrary
