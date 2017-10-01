2d69c32bd8e94f0607b7ec967b50df3aed390234
Brian Heim <brianlheim@gmail.com>
2017-10-01 10:24:54 -0400

    qpm: copy json file like before

--- .travis/qpm-prep.sh
+++ .travis/qpm-prep.sh
@@ -3,3 +3,4 @@
 cd $TRAVIS_BUILD_DIR/BUILD
 sudo pip install git+https://github.com/scztt/qpm.git@qpm-unit
 qpm quark checkout UnitTesting API --location $HOME/Quarks
+cp $TRAVIS_BUILD_DIR/.travis/travis_test_run.json .
