318b4a6d9e71d27935a910ce4307827a55e87966
Brian Heim <brianlheim@gmail.com>
2018-01-03 00:11:38 -0500

    travis: make qpm print output

--- .travis/qpm-test.sh
+++ .travis/qpm-test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 qpm test.run -l $TRAVIS_BUILD_DIR/BUILD/travis_test_run.json --path $SCLANG --include $HOME/Quarks \
-    $TRAVIS_BUILD_DIR/testsuite/classlibrary
+    $TRAVIS_BUILD_DIR/testsuite/classlibrary -o
