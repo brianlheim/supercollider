c8f514d30b984f3d3d495889d1b9fa130f83b83b
Brian Heim <brianlheim@gmail.com>
2018-12-12 23:50:44 -0600

    cmake_gen: add -Weverything for strict clang

--- tools/cmake_gen/SuperColliderCompilerConfig.cmake
+++ tools/cmake_gen/SuperColliderCompilerConfig.cmake
@@ -35,6 +35,10 @@ function(sc_config_compiler_flags target)
             $<$<BOOL:${NATIVE}>:-march=native>
             $<$<BOOL:${STRICT}>:-Wall -Wextra -Werror -Wpedantic>
             )
+        if(CMAKE_CXX_COMPILER_ID MATCHES "Clang|AppleClang" AND STRICT)
+            # the generator expression for this was too horrible
+            target_compile_options(${target} PUBLIC -Weverything)
+        endif()
     elseif(CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
         # these options only apply if we're doing a 32-bit build, otherwise they cause a diagnostic
         # https://stackoverflow.com/questions/1067630/sse2-option-in-visual-c-x64
