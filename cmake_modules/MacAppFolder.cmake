5a2404dd758ffd4d05997199be0d8ca030863219
Brian Heim <brianlheim@gmail.com>
2019-10-27 16:41:21 -0500

    CMake: improve variable name

--- cmake_modules/MacAppFolder.cmake
+++ cmake_modules/MacAppFolder.cmake
@@ -2,12 +2,12 @@ if(APPLE)
     ###### The user may set the app name, causing a standalone build
     ######	cmake -D standalone:string=MyFabApp ../
     if ("${standalone}" STREQUAL "")
-        set(scappbundlename SuperCollider)
+        set(sc_app_bundle_name SuperCollider)
     else()
         # We're building a standalone, change the app name.
-        set(scappbundlename ${standalone})
+        set(sc_app_bundle_name ${standalone})
         message(STATUS "Building sc in STANDALONE mode. App name: " ${standalone})
     endif()
 
-    set(sc_app_resources_dir "${scappbundlename}/${scappbundlename}.app/Contents/Resources" CACHE STRING "Installation path for the Resourcedir")
+    set(sc_app_resources_dir "${sc_app_bundle_name}/${sc_app_bundle_name}.app/Contents/Resources" CACHE STRING "Installation path for the Resourcedir")
 endif(APPLE)
