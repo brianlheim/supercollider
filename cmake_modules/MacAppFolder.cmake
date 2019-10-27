47372c73be28c33e23f290960e5e1cfb6ec002c4
Brian Heim <brianlheim@gmail.com>
2019-10-27 16:38:06 -0500

    CMake: simplify resource dir variables

--- cmake_modules/MacAppFolder.cmake
+++ cmake_modules/MacAppFolder.cmake
@@ -2,13 +2,12 @@ if(APPLE)
     ###### The user may set the app name, causing a standalone build
     ######	cmake -D standalone:string=MyFabApp ../
     if ("${standalone}" STREQUAL "")
-        set(scappbundlename ${PROJECT_NAME})
+        set(scappbundlename SuperCollider)
     else()
         # We're building a standalone, change the app name.
         set(scappbundlename ${standalone})
         message(STATUS "Building sc in STANDALONE mode. App name: " ${standalone})
     endif()
 
-    set(scappauxresourcesdir "${scappbundlename}/${scappbundlename}.app/Contents/Resources" CACHE STRING "Installation path for the Resourcedir")
-    set(scappbindir "${scappbundlename}/${scappbundlename}.app/Contents/MacOS" CACHE STRING "Installation path for the Bindir")
+    set(sc_app_resources_dir "${scappbundlename}/${scappbundlename}.app/Contents/Resources" CACHE STRING "Installation path for the Resourcedir")
 endif(APPLE)
