621b11007d4cdadfbe8fc106f0c896185f1f8f5d
Brian Heim <brianlheim@gmail.com>
2020-03-05 19:32:19 -0600

    debugger: add secondary thread

--- lang/Debugger/Debugger.cpp
+++ lang/Debugger/Debugger.cpp
@@ -2,10 +2,35 @@
 
 #include "Debugger/Debugger.h"
 
+#include <thread>
+#include <atomic>
+
 namespace Debugger {
 
-void initialize() noexcept { DEBUG_DEBUGGER("hello"); }
+static std::thread debuggerThread;
+static std::atomic<bool> running { false };
 
-void finalize() noexcept { DEBUG_DEBUGGER("goodbye"); }
+/// Actual running thread for debugger
+static void debuggerThreadWork() {
+    DEBUG_DEBUGGER("starting work");
+    while (running) {
+        // TODO(Brian) use a condition variable + sleep_for to synchronize
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        DEBUG_DEBUGGER("debugger thread -- waking up");
+    }
+    DEBUG_DEBUGGER("stopping work");
+}
+
+void initialize() noexcept {
+    DEBUG_DEBUGGER("hello");
+    running = true;
+    debuggerThread = std::thread(debuggerThreadWork);
+}
+
+void finalize() noexcept {
+    DEBUG_DEBUGGER("goodbye");
+    running = false;
+    debuggerThread.join();
+}
 
 } // namespace Debugger
