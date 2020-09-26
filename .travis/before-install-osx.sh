6f7ad8c86cc233d7c650fcece4a814ca09088137
Brian Heim <brianlheim@gmail.com>
2020-09-26 10:11:57 -0500

    test: manually update on brew to avoid multiple repeat updates (?)

--- .travis/before-install-osx.sh
+++ .travis/before-install-osx.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 export HOMEBREW_NO_ANALYTICS=1
+export HOMEBREW_NO_INSTALL_CLEANUP=1
+export HOMEBREW_NO_AUTO_UPDATE=1
 
 PACKAGES="libsndfile portaudio ccache ninja fftw"
 if $USE_SYSLIBS; then
@@ -8,6 +10,7 @@ if $USE_SYSLIBS; then
     PACKAGES="$PACKAGES yaml-cpp"
 fi
 
+brew update --preinstall
 brew install $PACKAGES || brew install $PACKAGES || exit 1
 brew upgrade qt5 || exit 4
 brew link qt5 --force || exit 5
